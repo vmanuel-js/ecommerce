f8a4af7f472ca9a0295e589fa312b673221e77e6
Victor Jordan <vmjordansolis@gmail.com>
2026-03-08 20:46:28 -0500

    fix: changing the place of the root

--- backend/src/index.js
+++ backend/src/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Ok").status(200);
+  res.status(200).send("OK");
 });
 
 const connectToDB = async () => {
