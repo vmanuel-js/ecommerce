6e0aa45654ec55a22ec12cfd68f1047c95c694ea
Victor Jordan <vmjordansolis@gmail.com>
2026-03-08 20:40:25 -0500

    feat: create product model

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  code: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  status: {
+    type: Boolean,
+    required: true,
+  },
+  stock: {
+    type: Number,
+    required: true,
+  },
+  category: {
+    type: String,
+    required: true,
+  },
+  thumbnails: {
+    type: [String],
+    default: [],
+  },
+});
+
+const ProductModel = mongoose.model("Product", productSchema);
+export default ProductModel;
